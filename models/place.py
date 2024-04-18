72ded62f13a20a3d6339c3fe1310cc75f599be0f
Abdel mou <abdelrebel001@gmail.com>
2024-04-17 17:09:25 -0700

    editing user, place files

--- models/place.py
+++ models/place.py
@@ -1,18 +1,76 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
+"""Defines the Place class."""
+from models.base_model import BaseModel, Base
+from models.review import Review
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+place_amenity = Table(
+    'place_amenity',
+    Base.metadata,
+    Column(
+        'place_id',
+        String(60),
+        ForeignKey('places.id'),
+        primary_key=True,
+        nullable=False
+    ),
+    Column(
+        'amenity_id',
+        String(60),
+        ForeignKey('amenities.id'),
+        primary_key=True,
+        nullable=False
+    )
+)
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+class Place(BaseModel, Base):
+    """Represents a place in the application.
+
+    Attributes:
+        city_id (str): The ID of the city where the place is located.
+        user_id (str): The ID of the user who owns the place.
+        name (str): The name of the place.
+        description (str): A description of the place.
+        number_rooms (int): The number of rooms in the place.
+        number_bathrooms (int): The number of bathrooms in the place.
+        max_guest (int): The maximum number of
+        guests the place can accommodate.
+        price_by_night (int): The price per night for staying at the place.
+        latitude (float): The latitude coordinate of the place.
+        longitude (float): The longitude coordinate of the place.
+        reviews (relationship): Relationship with the Review model.
+        Includes cascade delete to delete associated reviews.
+        amenities (relationship): Relationship with the Amenity
+        model through a many-to-many table.
+        Includes a backref to access places associated
+        with each amenity.
+    """
+    __tablename__ = "places"
+
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    reviews = relationship("Review", cascade="all, delete", backref="place")
+    amenities = relationship("Amenity", secondary=place_amenity,
+                             viewonly=False, backref="places")
     amenity_ids = []
+
+    @property
+    def reviews(self):
+        """ Returns the list of Review instances with
+        place_id == current Review.id """
+        list_reviews = []
+        place_reviews = models.engine.all(Review)
+        for pl_reviews in place_reviews.values():
+            if pl_reviews.place_id == self.id:
+                list_reviews.append(pl_reviews)
+        return list_reviews
--- models/user.py
+++ models/user.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""Defines the User class."""
+from models.base_model import BaseModel, Base
+from models.place import Place
+from models.review import Review
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """Represents a user in the application.
+
+    Attributes:
+        email (str): The email address of the user.
+        password (str): The password for user login.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+        places (relationship): Relationship with the Place model.
+        Includes cascade delete to delete associated places.
+        reviews (relationship): Relationship with the Review model.
+        Includes cascade delete to delete associated reviews.
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade="all, delete", backref="user")
+    reviews = relationship("Review", cascade="all, delete", backref="user")
